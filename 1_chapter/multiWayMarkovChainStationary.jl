39506ba3e1f1c836afe1d60ac249b24f84281a6d
Vincent Tam <sere@live.hk>
2019-05-10 10:38:01 +0200

    Julia JSON & MCMC

--- 1_chapter/multiWayMarkovChainStationary.jl
+++ 1_chapter/multiWayMarkovChainStationary.jl
@@ -1,28 +1,28 @@
-using LinearAlgebra, StatsBase
+using LinearAlgebra, StasBase
 
 # Transition probability matrix
-P = [0.5 0.4 0.1;
-     0.3 0.2 0.5;
-     0.5 0.3 0.2]
+P = [0.2 0.3 0.5;
+     0.7 0.1 0.2;
+     0.2 0.4 0.4]
 
-# First way
-piProb1 = (P^100)[1,:]
+# First way (very quick: 0.010701)
+@time piProb1 = (P^10000000000)[1,:]
 
-# Second way
+# Second way (analytical solution)
 A = vcat((P' - Matrix{Float64}(I, 3, 3))[1:2,:],ones(3)')
 b = [0 0 1]'
 piProb2 = A\b
 
-# Third way
+# Third way (analytical solution)
 eigVecs = eigvecs(copy(P'))
 highestVec = eigVecs[:,findmax(abs.(eigvals(P)))[2]]
 piProb3 = Array{Float64}(highestVec)/norm(highestVec,1);
 
-# Fourth way
+# Fourth way (slow and inaccurate: 8s for N = 10^7, err: 1e-4)
 numInState = zeros(3)
 state = 1
-N = 10^6
-for t in 1:N
+N = 10^7
+@time for t in 1:N
     numInState[state] += 1
     global state = sample(1:3,weights(P[state,:]))
 end
--- 1_chapter/shakespeareWords.jl
+++ 1_chapter/shakespeareWords.jl
@@ -1,18 +1,18 @@
 using HTTP, JSON
 
-data = HTTP.request("GET", 
-"https://ocw.mit.edu/ans7870/6/6.006/s08/lecturenotes/files/t8.shakespeare.txt")
-shakespeare = String(data.body)
-shakespeareWords = split(shakespeare)
+data = HTTP.request("GET",
+"http://www.gutenberg.org/files/53573/53573-0.txt")
+vandercook = String(data.body)
+vandercookWords = split(vandercook)
 
 jsonWords = HTTP.request("GET",
 "https://raw.githubusercontent.com/"*
 "h-Klok/StatsWithJuliaBook/master/1_chapter/jsonCode.json")
-parsedJsonDict = JSON.parse( String(jsonWords.body))
+parsedJsonDict = JSON.parse(String(jsonWords.body))
 
 keywords = Array{String}(parsedJsonDict["words"])
 numberToShow = parsedJsonDict["numToShow"]
-wordCount = Dict([(x,count(w -> lowercase(w) == lowercase(x), shakespeareWords))
+wordCount = Dict([(x,count(w -> lowercase(w) == lowercase(x), vandercookWords))
                   for x in keywords])
 
 sortedWordCount = sort(collect(wordCount),by=last,rev=true)
